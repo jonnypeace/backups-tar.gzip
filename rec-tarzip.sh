597d2cf8759935c36633e1f42cf9ecea21c98608
Jonny <royalpython2@hotmail.com>
2022-02-08 14:23:25 +0000

    Still testing these

---
+++ rec-tarzip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# incremental backup directory
+dirbk=/server-or-drive/that/stores/backups
+
+# Directory to recover incremental backups to
+# Example /home if you are recovering a users
+# home directory, i.e. /home/user
+recdir=/home
+
+for f in $(ls -tr $dirbk/*.tgz) ; do
+	cd $recdir && tar -x -g /dev/null -f $f
+done
---
+++ tarzip.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# incremental backup directory
+dirbk=/server/to/store/backups
+
+# directory to be backed up
+dirpass=/folder/to/be/backed/up
+
+# incremental file which keeps track of changing
+incfile=file.inc.sngz
+
+# date for directory and .tgz file naming
+day=$(date +'%F')
+
+#Check number of directories with week-ending, and count them
+dirnum=$(find $dirbk/*week-ending* -type d | wc -l)
+
+# My aim is to keep two weeks of backups at all times. 
+# If you want to adjust this, adjust the number 3 accordingly.
+# Example: 3 will keep 2 full weeks of dailing backups.
+if [[ $dirnum -ge 3 ]]; then
+	dir1=$(find $dirbk/*week-ending* -type d | sort | awk 'NR==1{print}')
+	rm -r $dir1;
+fi
+
+# Counting the number .tgz files
+filenum=$(find $dirbk/*.tgz -type f | wc -l)
+
+# Once 7 .tgz are created, move them to a new week-ending directory
+# If run daily on cron job, this will be a weeks worth of incremental backups
+if [[ $filenum -ge 7 ]]; then
+	mkdir -p $dirbk/week-ending-$day
+	mv $dirbk/*.tgz $dirbk/week-ending-$day
+	mv $dirbk/$incfile $dirbk/week-ending-$day
+fi
+
+# Create .tgz file. Ideally this will work in a cron job, and you'll get daily backups
+tar -vvcz -g $dirbk/$incfile -f $dirbk/pass-$day.tgz $dirpass
