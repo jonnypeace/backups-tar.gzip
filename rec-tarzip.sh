7afce6d03efc3cfc8b4e02f286bac6a42b6caafa
jonnypeace <royalpython2@hotmail.com>
2022-04-24 19:30:43 +0100

    modified:   rec-tarzip.sh

--- rec-tarzip.sh
+++ rec-tarzip.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # incremental backup directory
-dirbk=/server-or-drive/that/stores/backups
+dirbk=/backup-sc
 
 # Directory to recover incremental backups to
 # Example /home if you are recovering a users
 # home directory, i.e. /home/user
 recdir=/home
 
-for f in $(ls -tr "$dirbk"/*.tgz) ; do
+for f in $(find "$dirbk" -type f -iname "*.tgz" | awk -F/ 'i=split($0,a){print a[i] | "sort -n" }') ; do
 	cd "$recdir" && tar -x -g /dev/null -f "$f"
 done
